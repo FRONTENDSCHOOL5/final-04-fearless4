dadcd0a441bb35517beb59d68cc0388ac9645b0c
DayTeaJun <gkfnck@gmail.com>
2023-06-10 12:34:43 +0900

    Refactor: 각 style 컴포넌트 하나로 통합, Splash, Login 폴더로분할 button.style 및 logo.style 컴포넌트의 style.jsx를 각각 통합, loginJoin.style 에 FlexWrapper 생성, pages 폴더에 각 페이지별로 폴더 생성 및 페이지에 따른 Wrapper조절을 위해 style 컴포넌트 생성

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,15 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const Splash = lazy(() => import('../pages/Splash'));
-const Login = lazy(() => import('../pages/Login'));
+const Splash = lazy(() => import('../pages/splash/Splash'));
+const Login = lazy(() => import('../pages/login/Login'));
 
 const renderLoader = () => <span>로딩중...</span>;
 
 export default function Router() {
 	return (
 		<BrowserRouter basename=''>
-			<Suspense fallback={<Splash />}>
+			<Suspense fallback={renderLoader()}>
 				<Routes>
 					<Route path='/' element={<Login />} />
 				</Routes>
