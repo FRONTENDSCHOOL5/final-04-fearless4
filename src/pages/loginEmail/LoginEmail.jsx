8ca605e1c960f3d0cb4830b7c136aae8f7631881
최사라 <fktkchl@naver.com>
2023-06-10 14:17:35 +0900

    Docs: Login - email 페이지 마크업 마이그레이션 진행 button 폴더 안 button style 컴포넌트에 LoginButton style 추가, form 폴더 생성 및 폴더 안 form style 컴포넌트 생성, loginEmail 페이지 생성

---
+++ src/pages/loginEmail/LoginEmail.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+	WrapForm,
+	WrapEmailPw,
+	LabelStyle,
+	InputStyle,
+	Incorrect,
+} from '../../components/form/form.style.jsx';
+import {
+	WrapperLoginEmail,
+	Title,
+	SignUpContainer,
+} from './loginEmail.style.jsx';
+import { LoginButton } from '../../components/button/button.style.jsx';
+
+export default function LoginEmail() {
+	return (
+		<>
+			<WrapperLoginEmail>
+				<Title>로그인</Title>
+				<WrapForm>
+					<WrapEmailPw>
+						<LabelStyle htmlFor='user-email'>이메일</LabelStyle>
+						<InputStyle id='user-email' type='email' required />
+					</WrapEmailPw>
+					<WrapEmailPw>
+						<LabelStyle htmlFor='user-pw'>비밀번호</LabelStyle>
+						<InputStyle id='user-pw' type='password' required />
+						<Incorrect>*이메일 또는 비밀번호가 일치하지 않습니다.</Incorrect>
+						<LoginButton>로그인</LoginButton>
+					</WrapEmailPw>
+				</WrapForm>
+				<SignUpContainer>이메일로 회원가입</SignUpContainer>
+			</WrapperLoginEmail>
+		</>
+	);
+}
