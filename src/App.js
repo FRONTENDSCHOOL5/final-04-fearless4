596774fa0dd36055303a735097c23acb1ac57165
starcradle101 <dreamerdev12@gmail.com>
2023-06-10 13:32:08 +0900

    Fix: Main merge 충돌 해결 Main branch와 migrationProfileSetup branch merge시 충돌 해결

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import { GlobalStyle } from './GlobalStyle';
-<<<<<<< HEAD
-import Login from './pages/Login';
-import ProfileSetup from './pages/ProfileSetup';
-
-=======
 import Router from './routes/Router';
->>>>>>> main
 function App() {
 	return (
 		<>
 			<GlobalStyle />
-<<<<<<< HEAD
-			{/* <Splash /> */}
-			{/* <Login /> */}
-			<ProfileSetup />
-=======
 			<Router />
->>>>>>> main
 		</>
 	);
 }
