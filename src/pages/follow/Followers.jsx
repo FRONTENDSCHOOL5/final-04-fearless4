7f89043bec9e6f6a0fc78bb83b773fc6341522d3
DayTeaJun <gkfnck@gmail.com>
2023-06-13 14:20:09 +0900

    Feat: Followers 페이지에 팔로워 리스트 API 연결 확인 유저 팔로워 리스트를 불러오기 위한 API 연결

--- src/pages/follow/Followers.jsx
+++ src/pages/follow/Followers.jsx
@@ -14,13 +14,37 @@ import {
 	Wrapper,
 } from './follow.style';
 import { FollowButton } from '../../components/button/button.style';
+import axios from 'axios';
 
 export default function Follwers() {
+	const url = 'https://api.mandarin.weniv.co.kr';
+	const token =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYjRlMmQ2YjJjYjIwNTY2M2UxZGVhYSIsImV4cCI6MTY5MTMzODUwMSwiaWF0IjoxNjg2MTU0NTAxfQ.4g3BNHZUeVB-a5MXVgMLfX5jGNYnbs6NP0-fr4Jx3NM';
+
+	const followerData = async () => {
+		try {
+			const res = await axios({
+				method: 'GET',
+				url: `${url}/profile/camperz_/follower`,
+				headers: {
+					Authorization: `Bearer ${token}`,
+					'Content-type': 'application/json',
+				},
+			});
+			console.log(res);
+		} catch (error) {
+			console.log('에러입니다', error);
+		}
+	};
+
 	return (
 		<Wrapper>
 			<NavbarWrap>
 				<Backspace />
 				<NavbarTitle>Followers</NavbarTitle>
+				<button type='button' onClick={followerData}>
+					임시 팔로우 버튼
+				</button>
 			</NavbarWrap>
 
 			<UserWrap>
