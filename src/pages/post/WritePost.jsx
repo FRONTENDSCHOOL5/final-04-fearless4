7c32128e5cef6cff7b6bc0ced4542d126c86ee23
starcradle101 <dreamerdev12@gmail.com>
2023-06-14 15:46:01 +0900

    Docs: 게시글 작성 페이지 마크업 진행 중... 레이아웃 테스트를 위한 컴포넌트 추가

--- src/pages/post/WritePost.jsx
+++ src/pages/post/WritePost.jsx
@@ -7,12 +7,20 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { NavbarWrap } from '../../components/navbar/navbar.style';
+import {
+	Backspace,
+	NavbarWrap,
+	OptionModalTab,
+	NavbarTitle,
+} from '../../components/navbar/navbar.style';
 
 const WritePost = () => {
 	return (
 		<>
-			<NavbarWrap></NavbarWrap>
+			<NavbarWrap profile>
+				<Backspace />
+				<NavbarTitle>네비게이션 바 타이틀입니다</NavbarTitle>
+			</NavbarWrap>
 		</>
 	);
 };
