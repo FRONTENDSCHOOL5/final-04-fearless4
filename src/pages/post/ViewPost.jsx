b50c1bf8b1192e5b5b1e6c2f3a664bd5b7551179
starcradle101 <dreamerdev12@gmail.com>
2023-06-19 10:06:34 +0900

    Fix: 게시글 상세 페이지 작업 중 컴포넌트 마크업 버그 해결 * ViewPost.jsx의 UploadComment 컴포넌트가 뷰포트 밖으로 삐져나가는 버그 해결 * box-sizing: border-box; 속성을 주었음 Resolves: #26

--- src/pages/post/ViewPost.jsx
+++ src/pages/post/ViewPost.jsx
@@ -6,16 +6,37 @@ import {
 	OptionModalTab,
 } from '../../components/navbar/navbar.style';
 import ProfilePic from '../../assets/image/profilePic.png';
+import {
+	WrapperViewPost,
+	OptionModalTabComment,
+	PostView,
+	CommentSection,
+	UploadComment,
+	CommentInputArea,
+	CommentUploadButton,
+	ProfileImageComment,
+} from './ViewPost.style';
 
 const ViewPost = () => {
 	return (
-		<>
+		<WrapperViewPost>
 			<NavbarWrap spaceBetween>
 				<Backspace />
 				<OptionModalTab></OptionModalTab>
 			</NavbarWrap>
-		</>
+			<PostView>
+				<p>여기는 게시글 컴포넌트가 들어갈 자리입니다.</p>
+			</PostView>
+			<CommentSection></CommentSection>
+			<UploadComment>
+				<ProfileImageComment src={ProfilePic}></ProfileImageComment>
+				<CommentInputArea placeholder='여기에 텍스트 입력'></CommentInputArea>
+				<CommentUploadButton>게시</CommentUploadButton>
+			</UploadComment>
+		</WrapperViewPost>
 	);
 };
 
 export default ViewPost;
+
+//
--- src/pages/post/ViewPost.style.jsx
+++ src/pages/post/ViewPost.style.jsx
@@ -1,19 +1,84 @@
 import styled from 'styled-components';
+import { OptionModalTab } from '../../components/navbar/navbar.style';
 
 export const WrapperViewPost = styled.div`
 	width: 100%;
-	height: 100%;
-	display: flex;
 	position: relative;
 	box-sizing: border-box;
-	padding: 68px 16px 26px 16px;
-	gap: 13px;
 `;
 
-export const ProfileImageMini = styled.img`
+// 게시글 작성에도 동일한 컴포넌트가 사용되니 묶어서 정리할 것
+export const ProfileImagePost = styled.img`
 	display: block;
-	width: 44px;
-	height: 44px;
+	width: 42px;
+	height: 42px;
 	border-radius: 50%;
 	object-fit: cover;
+`;
+
+export const OptionModalTabComment = styled(OptionModalTab)`
+	width: 20px;
+	height: 20px;
+`;
+
+// 게시글 컴포넌트 삽입 전 임시로 작성
+export const PostView = styled.div`
+	width: 100%;
+	height: 434px;
+	background-color: #81d8d0;
+	display: flex;
+	padding: 68px 16px 26px 16px;
+	justify-content: center;
+	align-items: center;
+	border-bottom: 2px solid #dbdbdb;
+`;
+
+export const CommentSection = styled.section`
+	width: 100%;
+	height: auto;
+`;
+
+export const UploadComment = styled.div`
+	width: 100%;
+	box-sizing: border-box;
+	padding: 12px 16px;
+	border-top: 1px solid #dbdbdb;
+	position: fixed;
+	bottom: 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	gap: 18px;
+`;
+
+export const ProfileImageComment = styled.img`
+	display: block;
+	width: 36px;
+	height: 36px;
+	border-radius: 50%;
+	object-fit: cover;
+`;
+
+export const CommentInputArea = styled.textarea`
+	width: 260px;
+	resize: none;
+	border: none;
+
+	font-size: 14px;
+
+	&::placeholder {
+		font-size: 14px;
+	}
+	&:focus {
+		outline: none;
+	}
+`;
+
+export const CommentUploadButton = styled.button`
+	border: none;
+	background-color: #fff;
+	color: #81d8d0;
+	cursor: pointer;
+	font-size: 14px;
+	font-weight: 700;
 `;
