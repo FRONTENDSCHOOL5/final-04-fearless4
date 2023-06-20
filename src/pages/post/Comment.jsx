3d3b351e0ab44802e8610b16fe007cfff0c18491
starcradle101 <dreamerdev12@gmail.com>
2023-06-20 22:16:18 +0900

    Chore: moment 라이브러리 설치, Comment 컴포넌트 생성 * 댓글 및 게시글 작성 시간을 표시하기 위한 라이브러리 moment 설치 * 독립적인 댓글 컴포넌트 생성을 위한 Comment.jsx 생성 Related to: #41, #44

---
+++ src/pages/post/Comment.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+// import moment
+// import { CommentWrapper, FollowerProfileImageComment, CommentDetail, CommentFollower, CommentFollowerName, CommentTime, CommentText,OptionModalTabComment } from './viewPost.style';
