fd67dc78e88d450b4d7db14bf17c28beab12cff3
starcradle101 <dreamerdev12@gmail.com>
2023-06-20 22:24:24 +0900

    Docs: Comment 컴포넌트 작성 완료 Comment.jsx 파일 내부에 Comment 컴포넌트 작성 Related to: #41, #44

--- src/pages/post/Comment.jsx
+++ src/pages/post/Comment.jsx
@@ -1,3 +1,34 @@
 import React from 'react';
-// import moment
-// import { CommentWrapper, FollowerProfileImageComment, CommentDetail, CommentFollower, CommentFollowerName, CommentTime, CommentText,OptionModalTabComment } from './viewPost.style';
+import moment from 'moment';
+import {
+	CommentWrapper,
+	FollowerProfileImageComment,
+	CommentDetail,
+	CommentFollower,
+	CommentFollowerName,
+	CommentTime,
+	CommentText,
+	OptionModalTabComment,
+} from './viewPost.style';
+
+export const Comment = ({ comment }) => {
+	const { author, createdAt, content } = comment;
+	const fromNow = moment(createdAt).fromNow();
+
+	return (
+		<CommentWrapper>
+			<FollowerProfileImageComment
+				src={author.image}
+			></FollowerProfileImageComment>
+			<CommentDetail>
+				<CommentFollower>
+					<CommentFollowerName>{author.username}</CommentFollowerName>
+					<CommentTime>{fromNow}</CommentTime>
+				</CommentFollower>
+
+				<CommentText>{content}</CommentText>
+			</CommentDetail>
+			<OptionModalTabComment></OptionModalTabComment>
+		</CommentWrapper>
+	);
+};
