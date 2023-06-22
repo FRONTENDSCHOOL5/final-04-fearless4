49100e430154b4609d934ae4c30f2ac7712cec80
최사라 <fktkchl@naver.com>
2023-06-22 16:51:30 +0900

    Refactor: ProductsForSaleEdit 페이지에서 이미지 파일 업로드에 대한 유효성 검사 추가 업로드 할 이미지의 크기가 10MB가 넘거나 유효한 이미지 파일 확장자가 아닐 경우 업로드가 불가하게 함

--- src/pages/userProfile/ProductsForSaleEdit.jsx
+++ src/pages/userProfile/ProductsForSaleEdit.jsx
@@ -78,26 +78,61 @@ export default function ProductsForSaleEdit() {
 		setIsFormValid(isFormValid);
 	}, [selectedImage, productNameError, productPrice, salesLinkError]);
 
-	async function handleImageInputChange(e) {
-		const formData = new FormData();
+	const handleImageInputChange = async (e) => {
+		const allowedExtensionsRegex = /\.(jpg|gif|png|jpeg|bmp|tif|heic)$/i;
+		const maxImageSize = 10 * 1024 * 1024;
 		const imageFile = e.target.files[0];
-		console.log(imageFile);
-		formData.append('image', imageFile);
-		try {
-			const res = await axios({
-				method: 'POST',
-				url: 'https://api.mandarin.weniv.co.kr/image/uploadfile/',
-				data: formData,
-				headers: {
-					'Content-type': 'multipart/form-data',
-				},
-			});
-			const imageUrl = 'https://api.mandarin.weniv.co.kr/' + res.data.filename;
-			setSelectedImage(imageUrl);
-		} catch (error) {
-			console.error(error);
+
+		if (imageFile) {
+			if (imageFile.size > maxImageSize) {
+				alert(
+					'이미지 크기가 너무 큽니다. \n10MB보다 작은 이미지를 업로드 해주세요!'
+				);
+				e.target.value = '';
+				return;
+			}
+
+			const fileExtension = '.' + imageFile.name.split('.').pop().toLowerCase();
+			if (!allowedExtensionsRegex.test(fileExtension)) {
+				alert(
+					'올바른 파일 확장자가 아닙니다!\n올바른 파일 확장자는 다음과 같습니다: .jpg, .gif, .png, .jpeg, .bmp, .tif, .heic'
+				);
+				e.target.value = '';
+				return;
+			}
+
+			const formData = new FormData();
+			const reader = new FileReader();
+
+			formData.append('image', imageFile);
+
+			reader.onloadend = () => {
+				setSelectedImage(reader.result);
+			};
+
+			if (imageFile) {
+				reader.readAsDataURL(imageFile);
+			}
+
+			try {
+				const res = await axios({
+					method: 'POST',
+					url: 'https://api.mandarin.weniv.co.kr/image/uploadfile/',
+					data: formData,
+					headers: {
+						'Content-type': 'multipart/form-data',
+					},
+				});
+				const imageUrl =
+					'https://api.mandarin.weniv.co.kr/' + res.data.filename;
+				setSelectedImage(imageUrl);
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+			e.target.value = '';
 		}
-	}
+	};
 
 	async function handleSaveButtonClick(e) {
 		const productData = {
