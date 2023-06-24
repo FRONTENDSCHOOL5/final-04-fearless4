35242551e24a2d29d1f231a1dc54703f096153c3
DayTeaJun <gkfnck@gmail.com>
2023-06-24 23:46:20 +0900

    Docs: AppMobile을 만들어 앱을 모바일형태의 크기로 조절하도록 컴포넌트 추가 사용자가 모바일뷰로 보기 위해 UI를 추가 Fixes: #73

---
+++ src/mobile/AppMobile.style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MoblieWrap = styled.div`
+	display: flex;
+	justify-content: center;
+	background-color: #f5f5f5;
+	height: 100vh;
+`;
+
+export const MoblieWidth = styled.div`
+	width: 390px;
+	position: relative;
+`;
+
+export const MobileColor = styled.div`
+	background-color: #fff;
+`;
