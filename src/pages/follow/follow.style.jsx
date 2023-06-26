c8088b8f60eab3be27eb61cc838a6f784c04dd36
DayTeaJun <gkfnck@gmail.com>
2023-06-26 09:53:16 +0900

    Design: 채팅, 팔로우, 홈피드, 검색, 프로필 수정 페이지 내용 높이 조절 페이지에서 글이 가려지는 현상 수정 Fixes: #73

--- src/pages/follow/follow.style.jsx
+++ src/pages/follow/follow.style.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	width: 100%;
-	height: calc(100vh - 48px);
 	box-sizing: border-box;
 	padding: 24px 16px;
 	display: flex;
