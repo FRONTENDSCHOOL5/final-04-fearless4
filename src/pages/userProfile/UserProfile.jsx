0453bb3ca6515a4a5b53aea8e42d61984ec96b64
starcradle101 <dreamerdev12@gmail.com>
2023-06-26 11:43:17 +0900

    Fix: 상단 뒤로가기 버튼을 눌렀을 때의 기능 분할 * 기존에는 사용자가 게시글을 작성 -> 상세 페이지 이동 -> 수정 -> 상세 페이지 이동의 과정을 거친 후 뒤로가기 버튼을 누르면 해당 과정을 역순으로 반복했음. * 이런 불필요한 과정을 막기 위해서, 사용자의 이름을 비교하고 게시글 상세 페이지의 author.accountname과 현재 로그인한 사용자가 동일하다면 뒤로가기 버튼을 눌렀을 때 내 프로필로 이동하고, 다르다면 이전 페이지로 이동하는 기능을 추가

--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -35,7 +35,7 @@ import { BottomNavContainer } from '../../components/bottomnav/bottomnav.style';
 import axios from 'axios';
 
 import { API_URL } from '../../api';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import ProductsForSale from './ProductsForSale';
 export default function UserProfile() {
 	const location = useLocation();
