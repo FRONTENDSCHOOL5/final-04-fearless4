b29fb1e0349d2efaabb24a50a70a5e96d6014bf8
최사라 <fktkchl@naver.com>
2023-07-19 20:26:36 +0900

    Refactor: Product, ProductsForSaleEdit 컴포넌트에서 이미지 유효성 검사하는 부분을 새로운 컴포넌트 imageValidation로 분리 이미지 유효성 검사하는 부분을 새로운 컴포넌트인 imageValidation로 분리하여 코드의 중복을 줄이고 재사용성을 향상시킴 Resolves #114

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -26,6 +26,7 @@ import {
 	ToastMsgBold,
 } from '../../components/toast/toast.style';
 import { Helmet } from 'react-helmet';
+import imageValidation from '../../imageValidation';
 
 export default function Product() {
 	// 이미지 등록
@@ -59,47 +60,13 @@ export default function Product() {
 		setIsFormValid(isFormValid);
 	}, [selectedImage, productNameError, productPrice, salesLinkError]);
 
-	const handleImageInputChange = async (e) => {
-		const allowedExtensionsRegex = /\.(jpg|gif|png|jpeg|bmp|tif|heic)$/i;
-		const maxImageSize = 10 * 1024 * 1024;
-		const imageFile = e.target.files[0];
-		if (imageFile) {
-			if (imageFile.size > maxImageSize) {
-				setShowSizeOverToast(true);
-				setTimeout(() => setShowSizeOverToast(false), 3000);
-				e.target.value = '';
-				return;
-			}
-			const fileExtension = '.' + imageFile.name.split('.').pop().toLowerCase();
-			if (!allowedExtensionsRegex.test(fileExtension)) {
-				setShowWrongExtensionToast(true);
-				setTimeout(() => setShowWrongExtensionToast(false), 3000);
-				e.target.value = '';
-				return;
-			}
-
-			const formData = new FormData();
-
-			formData.append('image', imageFile);
-
-			try {
-				const res = await axios({
-					method: 'POST',
-					url: 'https://api.mandarin.weniv.co.kr/image/uploadfile/',
-					data: formData,
-					headers: {
-						'Content-type': 'multipart/form-data',
-					},
-				});
-				const imageUrl =
-					'https://api.mandarin.weniv.co.kr/' + res.data.filename;
-				setSelectedImage(imageUrl);
-			} catch (error) {
-				console.error(error);
-			}
-		} else {
-			e.target.value = '';
-		}
+	const handleImageInputChange = (e) => {
+		imageValidation(
+			e,
+			setSelectedImage,
+			setShowSizeOverToast,
+			setShowWrongExtensionToast
+		);
 	};
 
 	async function handleSaveButtonClick(e) {
--- src/pages/userProfile/ProductsForSaleEdit.jsx
+++ src/pages/userProfile/ProductsForSaleEdit.jsx
@@ -27,6 +27,7 @@ import {
 	ToastMsgBold,
 } from '../../components/toast/toast.style';
 import { Helmet } from 'react-helmet';
+import imageValidation from '../../imageValidation';
 
 export default function ProductsForSaleEdit() {
 	// 이미지 등록
@@ -84,49 +85,13 @@ export default function ProductsForSaleEdit() {
 		setIsFormValid(isFormValid);
 	}, [selectedImage, productNameError, productPrice, salesLinkError]);
 
-	const handleImageInputChange = async (e) => {
-		const allowedExtensionsRegex = /\.(jpg|gif|png|jpeg|bmp|tif|heic)$/i;
-		const maxImageSize = 10 * 1024 * 1024;
-		const imageFile = e.target.files[0];
-
-		if (imageFile) {
-			if (imageFile.size > maxImageSize) {
-				setShowSizeOverToast(true);
-				setTimeout(() => setShowSizeOverToast(false), 3000);
-				e.target.value = '';
-				return;
-			}
-
-			const fileExtension = '.' + imageFile.name.split('.').pop().toLowerCase();
-			if (!allowedExtensionsRegex.test(fileExtension)) {
-				setShowWrongExtensionToast(true);
-				setTimeout(() => setShowWrongExtensionToast(false), 3000);
-				e.target.value = '';
-				return;
-			}
-
-			const formData = new FormData();
-
-			formData.append('image', imageFile);
-
-			try {
-				const res = await axios({
-					method: 'POST',
-					url: 'https://api.mandarin.weniv.co.kr/image/uploadfile/',
-					data: formData,
-					headers: {
-						'Content-type': 'multipart/form-data',
-					},
-				});
-				const imageUrl =
-					'https://api.mandarin.weniv.co.kr/' + res.data.filename;
-				setSelectedImage(imageUrl);
-			} catch (error) {
-				console.error(error);
-			}
-		} else {
-			e.target.value = '';
-		}
+	const handleImageInputChange = (e) => {
+		imageValidation(
+			e,
+			setSelectedImage,
+			setShowSizeOverToast,
+			setShowWrongExtensionToast
+		);
 	};
 
 	async function handleSaveButtonClick(e) {
@@ -134,9 +99,6 @@ export default function ProductsForSaleEdit() {
 		const updatedItemName = productName.replace(/^\[[^\]]*\]\s*/, '');
 		const itemName = updatedCategory + ' ' + updatedItemName;
 
-		// if (category !== '일반') {
-		// 	itemName = '[' + category + ']' + ' ' + productName;
-		// }
 		const productData = {
 			product: {
 				itemName: itemName,
