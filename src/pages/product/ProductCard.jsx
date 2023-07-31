f14d6fcac0957593552c8466f50310b86d3a0ee6
최사라 <fktkchl@naver.com>
2023-07-31 16:36:36 +0900

    Feat: 상품 클릭시 새로운 ProductCard.jsx 뜨게 하는 기능 추가 상품 클릭시 새로운 ProductCard.jsx 뜨게 하는 기능 추가하여 더 크게 상품 이미지를 볼 수 있게 함 Resolves: #126

---
+++ src/pages/product/ProductCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+	ProductName,
+	ProductPrice,
+} from '../userProfile/productsForSale.style';
+import { OptionModalTab } from '../../components/navbar/navbar.style';
+import {
+	CloseIconImg,
+	CloseIconText,
+	ProductCardClose,
+	ProductCardContainer,
+	ProductCardImg,
+} from './productCard.style';
+import closeIcon from '../../assets/icon/icon-close.svg';
+
+export default function ProductCard({ item, handleCard, handleModal }) {
+	return (
+		<>
+			<ProductCardImg
+				src={item.itemImage}
+				alt={`${item.itemNamd}의 상품 이미지`}
+			/>
+			<ProductCardContainer>
+				<ProductName style={{ marginLeft: '10px', fontWeight: 'bold' }}>
+					{item.itemName}
+				</ProductName>
+				<OptionModalTab onClick={(e, boolean) => handleModal(e, true)} />
+			</ProductCardContainer>
+
+			<ProductPrice style={{ marginLeft: '10px', marginTop: '8px' }}>
+				{item.price.toLocaleString()}원
+			</ProductPrice>
+			<ProductCardClose onClick={() => handleCard(false)}>
+				<CloseIconImg src={closeIcon} alt='닫기' />
+				<CloseIconText>닫기</CloseIconText>
+			</ProductCardClose>
+		</>
+	);
+}
