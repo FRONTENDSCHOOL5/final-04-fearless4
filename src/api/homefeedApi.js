90a0877f9834a0afd64e39fde1ba36ea85526f59
sy412 <soyun04121@gmail.com>
2023-08-03 12:01:03 +0900

    [Feat] : Homefeed 페이지 api 파일 분리 및 axiosInstance 사용

---
+++ src/api/homefeedApi.js
@@ -0,0 +1,17 @@
+import { accessInstance } from './axiosInstance';
+
+const getHomefeed = async (pageParam) => {
+	const res = await accessInstance.get(
+		`/post/feed/?limit=10&skip=${pageParam}`
+	);
+	const { posts } = res.data;
+
+	return {
+		data: posts,
+		nextPage: pageParam,
+
+		// isLast: posts.length % 10 !== 0,
+	};
+};
+
+export default getHomefeed;
--- src/pages/homeFeed/Homefeed.jsx
+++ src/pages/homeFeed/Homefeed.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { PostDeleteContext } from '../post/PostDeleteContext.jsx';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import {
 	NavbarWrap,
 	TitleLogo,
 } from '../../components/navbar/navbar.style.jsx';
 import { HomefeedWrap, SearchIcon } from '../homeFeed/homefeed.style.jsx';
-import { API_URL } from '../../api.js';
 import HomeFollower from './HomeFollower';
 import NoFeed from './NoFeed.jsx';
 import { BottomNavContainer } from '../../components/bottomnav/bottomnav.style';
@@ -16,64 +14,38 @@ import { Helmet } from 'react-helmet';
 
 import { useInfiniteQuery } from '@tanstack/react-query'; //react-query
 import { useInView } from 'react-intersection-observer'; //라이브러리
+import getHomefeed from '../../api/homefeedApi.js';
 
 export default function Homefeed() {
-	const url = API_URL;
-	const token = localStorage.getItem('token');
 	const [deletedPostId, setDeletedPostId] = useState(null);
 	const navigate = useNavigate();
-
 	const [ref, inView] = useInView();
 	const count = useRef(0);
-
-	// 지정한 타겟 div가 화면에 보일 때 마다 서버에 요청
-	const getHomefeed = async (pageParam) => {
-		try {
-			const res = await axios({
-				method: 'GET',
-				url: `${url}/post/feed/?limit=10&skip=${pageParam}`,
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'Content-type': 'application/json',
-				},
-			});
-
-			const { posts } = res.data;
-
-			return {
-				data: posts,
-				nextPage: pageParam,
-				isLast: posts.length % 10 !== 0,
-			};
-		} catch (error) {
-			console.log('에러입니다');
-		}
-	};
 
 	const {
 		data: followingFeedData,
 		isLoading,
 		fetchNextPage,
+		hasNextPage,
 	} = useInfiniteQuery(
-		['getFollowingFeed'],
+		['getFeedPosts'],
 		({ pageParam = count.current }) => getHomefeed(pageParam),
 		{
 			getNextPageParam: (lastPage) => lastPage.nextPage + 10,
 		}
 	);
 
-	console.log(followingFeedData?.pages);
-	console.log(count.current);
-	console.log(inView);
-
 	useEffect(() => {
 		if (!isLoading) {
-			if (inView && !followingFeedData?.pages[count.current].isLast) {
+			if (inView) {
 				count.current += 1;
 				fetchNextPage();
 			}
 		}
 	}, [inView]);
+
+	console.log(followingFeedData?.pages);
+	console.log(inView);
 
 	return (
 		<>
@@ -106,13 +78,17 @@ export default function Homefeed() {
 						)}
 					</>
 				)}
-				<div ref={ref}></div>
 
 				{!isLoading && followingFeedData?.pages[0].data.length === 0 && (
 					<NoFeed />
 				)}
 				<BottomNavContainer home />
 				{isLoading && <Loading />}
+				{hasNextPage && (
+					<>
+						<div ref={ref} />
+					</>
+				)}
 			</HomefeedWrap>
 		</>
 	);
