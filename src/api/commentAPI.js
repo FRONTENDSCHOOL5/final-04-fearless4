3e53d9f0001426a4e8a0abb5b78fda1b3466b7e2
starcradle101 <dreamerdev12@gmail.com>
2023-08-25 03:43:27 +0900

    Refactor: commentAPI.js 함수 추가 commentAPI.js에서 댓글 목록을 불러오는 getCommentList 함수 추가 Related: #116

--- src/api/commentAPI.js
+++ src/api/commentAPI.js
@@ -1,5 +1,20 @@
 import { accessInstance } from './axiosInstance';
 
+export const getCommentList = async (postId) => {
+	try {
+		const response = await accessInstance.get(
+			`/post/${postId}/comments/?limit=infinity`
+		);
+		const sortedComments = response.data.comments.sort((a, b) => {
+			return new Date(a.createdAt) - new Date(b.createdAt);
+		});
+		return sortedComments;
+	} catch (error) {
+		console.log('오류 발생', error.response || error);
+		throw error;
+	}
+};
+
 export const deleteComment = async ({ postId, commentId }) => {
 	try {
 		await accessInstance.delete(`/post/${postId}/comments/${commentId}`);
